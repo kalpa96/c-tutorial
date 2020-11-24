2267c1e01361944a61960a7a04b55cdfa5542c9a
kalpa96 <65667945+kalpa96@users.noreply.github.com>
2020-11-24 21:35:49 +0530

    declare variables

---
+++ display3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int Age;
+	char First,secound,Third;
+	
+	Age=3;
+	First='A';
+	secound='C';
+	Third='T';
+	
+	printf("how old is your %c%c%c?\n",secound,First,Third);
+	printf("%d years",Age);
+	
+	return 0;
+}
