976f2d03945aba3c1f450d1c414a4bdc98158e00
kalpa96 <65667945+kalpa96@users.noreply.github.com>
2020-11-24 21:26:56 +0530

    display name,index,address,dob

---
+++ display1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int a=18001483;
+	char Fname[10]="Kalpa";
+	char Sname[15]="Sandaruwan";
+	char Add[30]="No 22, galgamuwa,Keppitiwalana";
+	char Dob[15]="29.06.1996";
+	
+	printf("Name : %s %s\n",Fname,Sname);
+	printf("Index Number : %d\n",a);
+	printf("Address : %s\n",Add);
+	printf("Date of Birth : %s",Dob);
+	
+	return 0;
+}
