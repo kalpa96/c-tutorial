843c34b70fc151c3143d596811b3ba09ef3d00e9
kalpa96 <65667945+kalpa96@users.noreply.github.com>
2020-11-28 20:44:17 +0530

    create new pattern2

---
+++ pattern2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j,k;
+	for(i=1;i<=6;i++)
+	{
+		for(j=5;i<=j;j--)
+		{
+			printf(" ");
+		}
+			for(k=1;k<=i;k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
